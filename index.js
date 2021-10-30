f0ddff107096016b60ee8dc0c64e920ffe3c857d
Ibrahim193084 <ibrahimibr193084@gmail.com>
2021-10-30 15:18:42 +0600

    sixth

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ async function run() {
   const id = req.params.id;
   const qurey = {_id: ObjectId(id)};
   const result = await bookCollection.deleteOne(qurey)
-  console.log('deleting booking with id', id, result);
   res.json(result);
 })
     } finally {
