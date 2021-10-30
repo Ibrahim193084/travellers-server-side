dca308ce4003faa4b672f5b750783565a6ea62e8
Ibrahim193084 <ibrahimibr193084@gmail.com>
2021-10-31 01:55:59 +0600

    11th

--- index.js
+++ index.js
@@ -67,21 +67,20 @@ async function run() {
   res.json(result);
 })
 
-   //Update api for update status
-   app.put('/book/:id', async (req, res) => {
-    const id = req.params.id;
-    const updateStatus = req.body;
-    const filter = { _id: ObjectId(id) };
-    const options = { upsert: true };
-    const updateDoc = {
-        $set: {
-           status: updateStatus
-        },
-    };
-    const result = await bookCollection.updateOne(filter, updateDoc, options)
-    console.log('updating', id)
-    res.json(result)
-})
+//    //Update api for update status
+//    app.put('/book/:id', async (req, res) => {
+//     const id = req.params.id;
+//     const filter = { _id: ObjectId(id) };
+//     const options = { upsert: true };
+//     const updateDoc = {
+//         $set: {
+//             status: "Approved"
+//         },
+//     };
+//     const result = await bookCollection.updateOne(filter, updateDoc, options)
+//     console.log('updating', id)
+//     res.json(result)
+// })
 
     } finally {
     //   await client.close();
